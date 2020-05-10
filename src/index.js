bebcac4877ffba9ce1a0a82906752a1ac7f5a0cf
jsaarest <39669220+jsaarest@users.noreply.github.com>
2020-05-10 11:51:37 +0300

    Added HashRouter to Index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {HashRouter, Route} from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <HashRouter>
+    <Route path="/" component={App}/>
+  </HashRouter>,
   document.getElementById('root')
 );
 
