8e174e9644bc34efe276e4933bd130a3dbfce56c
jsaarest <39669220+jsaarest@users.noreply.github.com>
2020-05-10 10:30:33 +0300

    Updates

---
+++ precache-manifest.f00f432db9ecb542c0bfef0ac4ed0623.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3804befc36219089cec47a09b7cf9e1e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6ca72ffad24651aec65e",
+    "url": "/portfolio/static/css/main.11a9c5b3.chunk.css"
+  },
+  {
+    "revision": "9f199f557a2adb9f7239",
+    "url": "/portfolio/static/js/2.99874f28.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.99874f28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca72ffad24651aec65e",
+    "url": "/portfolio/static/js/main.9a2923b2.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "8b592b4f69a0bd808cfdbcd021e2846d",
+    "url": "/portfolio/static/media/avatar-joona.8b592b4f.png"
+  },
+  {
+    "revision": "2aa030977292e25e8782b3aa90d9211d",
+    "url": "/portfolio/static/media/bankify_logo_Big_navy.2aa03097.png"
+  },
+  {
+    "revision": "6d5679da11aa05134c217aecfe63238a",
+    "url": "/portfolio/static/media/drinkster.6d5679da.png"
+  },
+  {
+    "revision": "9ea5c20763eb47b1f427255dd218ff59",
+    "url": "/portfolio/static/media/drinkster_logo_square.9ea5c207.png"
+  },
+  {
+    "revision": "5e364c095b6730e9b8edb78444bedf05",
+    "url": "/portfolio/static/media/elastig-logo-square.5e364c09.png"
+  },
+  {
+    "revision": "03ca58aa37dd83d8938692fdaecbf08c",
+    "url": "/portfolio/static/media/group-payments.03ca58aa.png"
+  },
+  {
+    "revision": "92de2486e9a027a36b7b20572807a030",
+    "url": "/portfolio/static/media/image-elastic.92de2486.jpg"
+  },
+  {
+    "revision": "dd9b2c1c540238082586bf7d18dab07c",
+    "url": "/portfolio/static/media/logo.dd9b2c1c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.481bf74c336d9f0319edef8fda4d263f.js"
+  "/portfolio/precache-manifest.f00f432db9ecb542c0bfef0ac4ed0623.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
